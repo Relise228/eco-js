ad663aa25bd125dd0794eb342d29cef2bb041725
IliyaSavin <49229895+IliyaSavin@users.noreply.github.com>
2021-03-26 13:50:37 +0200

    access control header

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,12 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice';
 import stationsReducer from '../features/stationsSlice';
-import {reducer as formReducer} from 'redux-form';
+//import {reducer as formReducer} from 'redux-form';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     stations: stationsReducer,
-    form: formReducer,
+    //form: formReducer,
   },
 });
